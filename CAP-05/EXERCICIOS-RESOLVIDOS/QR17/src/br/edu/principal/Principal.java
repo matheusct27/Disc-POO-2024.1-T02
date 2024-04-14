d49d2b705a2defc340bedcc3ce0bca32ff5df888
Matheus <41300255+matheusct27@users.noreply.github.com>
2024-04-14 11:25:28 -0300

    Add files via upload

---
+++ CAP-05/EXERCICIOS-RESOLVIDOS/QR17/src/br/edu/principal/Principal.java
@@ -0,0 +1,26 @@
+package br.edu.principal;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int meses = 0;
+		double sal_carlos, sal_joao;
+		
+		System.out.println("Digite o salário de carlos: ");
+		sal_carlos = sc.nextDouble();
+		
+		sal_joao = sal_carlos / 3;
+		
+		while(sal_joao < sal_carlos) {
+			sal_carlos = sal_carlos + (sal_carlos * 2/100);
+			sal_joao = sal_joao + (sal_joao * 5/100);
+			meses = meses + 1;
+		}
+		System.out.println("A quantidade de meses para que João iguale ou ultrapasse Carlos é: " + meses);
+
+	}
+
+}
